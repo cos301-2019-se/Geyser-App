ce9dc98d1eecacfc6dbb01c2a5fdc124f0ebb360
Alexandros Petrou <alexpetrou94@gmail.com>
2019-05-14 18:16:59 +0200

    -add skeletal logon screen -add skeletal firebase support to log in into the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBHGMLxlFq8i0QS1eDm4gckQ9JtJdGXL_g",
+    authDomain: "geyserapp-5ef72.firebaseapp.com",
+    databaseURL: "https://geyserapp-5ef72.firebaseio.com",
+    projectId: "geyserapp-5ef72",
+    storageBucket: "geyserapp-5ef72.appspot.com",
+    messagingSenderId: "303262406186",
+    appId: "1:303262406186:web:97ac0cac3d72b3eb"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBHGMLxlFq8i0QS1eDm4gckQ9JtJdGXL_g",
+    authDomain: "geyserapp-5ef72.firebaseapp.com",
+    databaseURL: "https://geyserapp-5ef72.firebaseio.com",
+    projectId: "geyserapp-5ef72",
+    storageBucket: "geyserapp-5ef72.appspot.com",
+    messagingSenderId: "303262406186",
+    appId: "1:303262406186:web:97ac0cac3d72b3eb"
+  }
 };
 
 /*
