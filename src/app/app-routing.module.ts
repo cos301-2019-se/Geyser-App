deafb570a90a7b6108473055b11056b92b0d054f
Zi Xin Zhang <x.xing.zhang@gmail.com>
2019-05-21 21:04:20 +0200

    Implement Barcode Scanner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   //{ path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: '', loadChildren: './barcode-scanner/barcode-scanner.module#BarcodeScannerPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
+  { path: 'barcode-scanner', loadChildren: './barcode-scanner/barcode-scanner.module#BarcodeScannerPageModule' },
+
 ];
 
 @NgModule({
